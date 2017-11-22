0e5a3530de4a62b929fc9345fc33908d442b69dd
Unknown <franklin.75@wright.edu>
2017-11-21 19:25:59 -0500

    Branch test, code is still broken
    Working on PhotoFromGallery

--- NOS_Android_App/app/src/main/java/ceg4110/nos_android_app/PhotoFromGallery.java
+++ NOS_Android_App/app/src/main/java/ceg4110/nos_android_app/PhotoFromGallery.java
@@ -1,15 +1,20 @@
 package ceg4110.nos_android_app;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.net.ConnectivityManager;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 
@@ -58,6 +63,66 @@ public class PhotoFromGallery extends AppCompatActivity {
     }
 
     public void onClickAssess(View view) {
+        final String result1;
+        final String[] result;
         //do the stuff to assess the image for food
+        if (((ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo() != null) {
+            new AsyncTask<Void, Integer, Boolean>() {
+
+                ProgressDialog progressDialog;
+
+                @Override
+                protected void onPreExecute() {
+                    super.onPreExecute();
+                    progressDialog = new ProgressDialog(UploadPhotoMenu.this);
+                    progressDialog.setMessage("Assessing food content...");
+                    progressDialog.show();
+                }
+
+                @Override
+                protected Boolean doInBackground(Void... params) {
+                    Log.i(TAG, "Entering Uploader");
+                    result1 = uploader.uploadFile(name, mCurrentPhotoPath);
+                    result = uploader.getAllResults();
+
+                    if (result1.equals(""))
+                        return false;
+                    else
+                        return true;
+                }
+
+                @Override
+                protected void onPostExecute(Boolean aBoolean) {
+                    super.onPostExecute(aBoolean);
+                    if (progressDialog != null)
+                        progressDialog.dismiss();
+
+                    Log.i(TAG, "Entering Results Screen...");
+                    goToResults();
+
+                    if (aBoolean) {
+                        Log.i(TAG, "Upload succeeded");
+                    }
+                    else {
+                        Log.i(TAG, "Upload failed");
+                        Toast.makeText(getApplicationContext(), "Upload error! Moving photo to Pending folder", Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(mContext, PendingMenuFolder.class);
+                        intent.putExtra("photoPath", mCurrentPhotoPath);
+                        startActivity(intent);
+
+                    }
+                }
+            }.execute();
+        } else {
+            Toast.makeText(getApplicationContext(), "No Internet Connection", Toast.LENGTH_LONG).show();
+        }
     }
+
+    public void goToResults(){
+        Intent intent = new Intent(this, ResultScreen.class);
+        intent.putExtra("photoPath", mCurrentPhotoPath);
+        intent.putExtra("resultNums", result[1]);
+        intent.putExtra("resultAns", result[2]);
+        startActivity(intent);
     }
+}
